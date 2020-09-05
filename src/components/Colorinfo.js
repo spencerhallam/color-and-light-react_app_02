bf34461fb776a7b0d24819d36a9a6655c6d74f46
spencerhallam <spencerhallam@gmail.com>
2020-09-05 16:11:09 -0500

    test commit for bash

--- src/components/Colorinfo.js
+++ src/components/Colorinfo.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Colorinfo(props) {
 
     return (
-    <div className="color-value">
+    <div className="color-values">
         <h5>{props.colortitle}</h5>
         <span style={{color : props.colordata}}>&#9608; </span>
         <span>{props.colordata}</span>
