76cf78b71f2ddd7521afa1e68c31773ecc54b986
spencerhallam <spencerhallam@gmail.com>
2020-09-06 08:22:48 -0500

    Updates

---
+++ precache-manifest.8c04a404d19d6f5d6d44d1829a4ba376.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ae8428aa77e414cad04dca8ed2ee79",
+    "url": "/color-and-light-react_app_02/index.html"
+  },
+  {
+    "revision": "520980f7a5ad78485a1e",
+    "url": "/color-and-light-react_app_02/static/css/main.e464d942.chunk.css"
+  },
+  {
+    "revision": "2eb553d6ea42469f78ba",
+    "url": "/color-and-light-react_app_02/static/js/2.e2503341.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/color-and-light-react_app_02/static/js/2.e2503341.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520980f7a5ad78485a1e",
+    "url": "/color-and-light-react_app_02/static/js/main.f025537d.chunk.js"
+  },
+  {
+    "revision": "ccdc80f9b1090c4977b5",
+    "url": "/color-and-light-react_app_02/static/js/runtime-main.0ae8af4b.js"
+  }
+]);
